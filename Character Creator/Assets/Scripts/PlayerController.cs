2a31b4b127641b80440b6aa297eaf161f857e88b
jan3tl <j1lim2023@chadwickschool.org>
2021-12-07 10:37:18 -0800

    work on challenge 2

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public SpriteRenderer ArmorRenderer;
+   public SpriteRenderer WeaponRenderer;
 
    public void SetArmor(Sprite newSprite)
    {
        ArmorRenderer.sprite = newSprite;
    }
+
+   public void SetWeapon (Sprite newWeapon)
+   {
+       WeaponRenderer.sprite = newWeapon;
+   }
+   
     void Start()
     {
         
