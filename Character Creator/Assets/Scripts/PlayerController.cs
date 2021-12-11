43af7605684be41b559d8ad5a8f2abafb29b1581
jan3tl <j1lim2023@chadwickschool.org>
2021-12-11 13:43:43 -0800

    feat: magic shop

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public SpriteRenderer ArmorRenderer;
-   public SpriteRenderer WeaponRenderer;
-   public GameObject WeaponShop;
-   public GameObject ArmorShop;
+    public SpriteRenderer ArmorRenderer;
+    public SpriteRenderer WeaponRenderer;
+    public GameObject MagicRenderer;
+    public ParticleSystem Bacteria;
+    public ParticleSystem Cryokinesis;
+    public ParticleSystem Pyrokinesis;
+    public ParticleSystem Hydrokinesis;
+    
 
-   public void SetArmor(Sprite newSprite)
-   {
-       ArmorRenderer.sprite = newSprite;
-   }
 
-   public void SetWeapon (Sprite newWeapon)
-   {
-       WeaponRenderer.sprite = newWeapon;
-   }
-   
+
+    public void SetArmor(Sprite newSprite)
+    {
+        ArmorRenderer.sprite = newSprite;
+    }
+
+    public void SetWeapon(Sprite newWeapon)
+    {
+        WeaponRenderer.sprite = newWeapon;
+    }
+
     void Start()
     {
-        
+        //this.MagicRenderer.gameObject.SetActive(false);
+        this.Cryokinesis.Stop(true);
+        this.Hydrokinesis.Stop(true);
+        this.Pyrokinesis.Stop(true);
+        this.Bacteria.Stop(true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
